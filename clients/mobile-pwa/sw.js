0d5eba15378ca32b295012ec0c451657c7f93412
Admin-Adher <215896237+Admin-Adher@users.noreply.github.com>
2026-06-13 19:07:18 +0200

    Fix cloud device registration reuse

--- clients/mobile-pwa/sw.js
+++ clients/mobile-pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norva-mobile-v6';
+const CACHE = 'norva-mobile-v7';
 const ASSETS = ['/', '/index.html', '/account.html', '/cloud.html', '/manifest.json', '/icon-192.png', '/cloudApi.js', '/authApi.js'];
 
 self.addEventListener('install', e =>
