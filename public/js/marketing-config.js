12f7d98ec67a70e8a06cc2d08fd24f2377d24216
Admin-Adher <adrienhernandez20@gmail.com>
2026-07-14 14:25:58 +0200

    Preserve TV VOD timeframe duration

---
+++ public/js/marketing-config.js
@@ -0,0 +1,21 @@
+window.NORVA_MARKETING_CONFIG = window.NORVA_MARKETING_CONFIG || {
+  enabled: false,
+  consentMode: 'granted',
+  debug: false,
+  googleAnalytics: {
+    measurementId: '',
+    sendPageView: true
+  },
+  googleAds: {
+    conversionId: '',
+    conversions: {
+      signup: '',
+      beginCheckout: '',
+      trialStart: '',
+      purchase: ''
+    }
+  },
+  meta: {
+    pixelId: ''
+  }
+};
